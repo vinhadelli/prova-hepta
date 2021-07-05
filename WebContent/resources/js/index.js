40a25aa12a3991eb24e86d8c9ccdc52c49267511
Gustavo Ruiz <gustavo.oliveira@hepta.com.br>
2021-07-05 11:01:08 -0300

    Correções baseadas em feedback

--- WebContent/resources/js/index.js
+++ WebContent/resources/js/index.js
@@ -1,24 +1,17 @@
 var inicio = new Vue({
 	el:"#inicio",
     data: {
-        listaProdutos: [],
-        listaProdutosHeader: [
-			{sortable: false, key: "nome", label:"Nome"},
-			{sortable: false, key: "fabricante.nome", label:"Fabricante"},
-			{sortable: false, key: "volume", label:"Volume"},
-			{sortable: false, key: "unidade", label:"Unidade"},
-			{sortable: false, key: "estoque", label:"Estoque"}
-		]
+        lista: []
     },
     created: function(){
         let vm =  this;
-        vm.buscaProdutos();
+        vm.listarFuncionarios();
     },
     methods:{
-        buscaProdutos: function(){
+        listarFuncionarios: function(){
 			const vm = this;
 			axios.get("/funcionarios/rs/funcionarios")
-			.then(response => {vm.listaProdutos = response.data;
+			.then(response => {vm.lista = response.data;
 			}).catch(function (error) {
 				vm.mostraAlertaErro("Erro interno", "Não foi listar natureza de serviços");
 			}).finally(function() {
